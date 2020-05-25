155c0cbf1e4045c53679af102cf93fcebe4eee73
keisuke.kato <kgulliverk@hotmail.com>
2020-05-25 22:42:11 +0900

    devise導入

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ ruby '2.6.5'
 
 gem 'carrierwave'
 gem 'mini_magick'
-
+gem 'devise'
+gem 'devise-i18n'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4'
 # Use postgresql as the database for Active Record
@@ -51,6 +52,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'letter_opener_web'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  path: 'users',
+  path_names: { sign_in: :login, sign_out: :logout }
+  # controllers: {
+  #   confirmations: 'users/confirmations',
+  #   passwords: 'users/passwords',
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions'
+  # }
+
   get 'stories/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :stories do
+    collection do
+      post :confirm
+      get :mystory
+    end
+  end
+
+  resources :parts do
+    collection do
+      post :confirm
+    end
+  end
+  root 'stories#index'
+  
+
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
