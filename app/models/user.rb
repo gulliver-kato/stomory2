44d0cc019eceff5c3f93b830cf0998196208b693
keisuke.kato <kgulliverk@hotmail.com>
2020-05-26 20:20:12 +0900

    rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   validates :gender, presence: true
   validates :email, presence: true, length: {maximum: 255},
               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  before_validation { email.downcase! }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+#   before_validation { email.downcase! }
+#   has_secure_password
+#   validates :password, presence: true, length: { minimum: 6 }
   has_many :stories
 
   enum gender: { 男性:1, 女性:2 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users,
   path: 'users',
   path_names: { sign_in: :login, sign_out: :logout }
+  # controllers: {
+  #   passwords: 'users/passwords',
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions'
+  # }
+
+  root 'stories#index'
+
+  resources :users, only: [:show]
 
   resources :stories do
     collection do
@@ -18,7 +26,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  root 'stories#index'
   
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
