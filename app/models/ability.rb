2f67068bda258a9f06f00a5e1e079547b32c2d16
keisuke.kato <kgulliverk@hotmail.com>
2020-05-27 09:15:16 +0900

    admin修正

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,7 @@ class Ability
   def initialize(user)
     if user.try(:admin?)
       can :access, :rails_admin
+      can :read, :dashboard
       can :manage, Story
       can :manage, User
     end
